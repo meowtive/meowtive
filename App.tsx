3cd2d9497bc94dcc11a8735842c6227b66194c2a
Filipi Rafael <filipirafael.123@gmail.com>
2024-07-16 13:14:37 -0300

    feat: update app with unistyles test styling

--- App.tsx
+++ App.tsx
@@ -1,19 +1,28 @@
 import {useEffect} from 'react';
-import SplashScreen from 'react-native-splash-screen';
 import {SafeAreaView, StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import {OnboardingScreen} from '@/screens/OnboardingScreen';
+import {createStyleSheet, useStyles} from 'react-native-unistyles';
 
 function App(): React.JSX.Element {
+  const {styles} = useStyles(stylesheet);
+
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <StatusBar barStyle="default" />
       <OnboardingScreen />
     </SafeAreaView>
   );
 }
+
+const stylesheet = createStyleSheet({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
