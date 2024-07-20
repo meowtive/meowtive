445f20a3d13b63cf14ca0d3c754a05eb588997a9
Filipi Rafael <filipirafael.123@gmail.com>
2024-07-20 16:01:31 -0300

    chore: remove splash-screen and install boot-splash and also upgrade yarn version

--- App.tsx
+++ App.tsx
@@ -1,15 +1,9 @@
-import {useEffect} from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
 import {OnboardingScreen} from '@/screens/OnboardingScreen';
 import {createStyleSheet, useStyles} from 'react-native-unistyles';
 
 function App(): React.JSX.Element {
   const {styles} = useStyles(stylesheet);
-
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
