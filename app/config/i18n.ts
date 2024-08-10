d0bc5641ef35a59ef2a261aaf2efc1fca768ff01
gustavoabel <gustavo.abel@totvs.com.br>
2024-08-10 12:24:52 -0300

    refactor: improve i18n imports and constant

--- app/config/i18n.ts
+++ app/config/i18n.ts
@@ -2,11 +2,9 @@ import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import * as RNLocalize from 'react-native-localize';
 
-import { en } from '@resources/strings/en';
-import { es } from '@resources/strings/es';
-import { pt } from '@resources/strings/pt';
+import * as i18n from '@resources';
 
-const resources = { en, es, pt };
+const resources = { i18n };
 
 i18next.use(initReactI18next).init(
   {
