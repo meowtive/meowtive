cdb3a70471e745a9ad89af53fad1d79b20f22d43
gustavoabel <gustavo.abel@totvs.com.br>
2024-08-12 17:45:15 -0300

    fix: quotes not showing

--- app/config/i18n.ts
+++ app/config/i18n.ts
@@ -2,9 +2,9 @@ import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import * as RNLocalize from 'react-native-localize';
 
-import * as i18n from '@resources';
+import { en, es, pt } from '@resources';
 
-const resources = { i18n };
+const resources = { en, es, pt };
 
 i18next.use(initReactI18next).init(
   {
