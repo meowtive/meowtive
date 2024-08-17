7fd81f1047d4b33395ee0e0a9858d5c295ce31f4
gustavoabel <gustavo.abel@totvs.com.br>
2024-08-16 21:30:49 -0300

    feat: setup bottom tab navigator

---
+++ app/routes/Routes.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/no-unstable-nested-components */
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen, OnboardingScreen } from '@screens';
+import { NavigationContainer } from '@react-navigation/native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const Tab = createBottomTabNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: () => (
+              <FontAwesome name="home" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Favorite"
+          component={OnboardingScreen}
+          options={{
+            tabBarIcon: () => (
+              <MaterialIcons name="favorite" size={24} color="black" />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
