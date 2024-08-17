a5a4d870ba7227213204e536d81aaff49505a140
gustavoabel <gustavo.abel@totvs.com.br>
2024-08-16 21:31:04 -0300

    feat: add routes on app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import { HomeScreen } from '@screens';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 import { I18nextProvider } from 'react-i18next';
 import i18next from '@config/i18n';
 import RNBootSplash from 'react-native-bootsplash';
 import { isAndroid } from '@config/platform';
+import Routes from '@routes/Routes';
 
 const App = () => {
   const { styles } = useStyles(stylesheet);
@@ -20,7 +20,7 @@ const App = () => {
     <I18nextProvider i18n={i18next}>
       <SafeAreaView style={styles.container}>
         <StatusBar barStyle="default" />
-        <HomeScreen />
+        <Routes />
       </SafeAreaView>
     </I18nextProvider>
   );
