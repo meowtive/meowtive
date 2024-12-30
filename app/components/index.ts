eb295b233a764bb95cecd3c73ec932360c454b52
filipirafael.123@gmail.com <filipirafael.123@gmail.com>
2024-12-30 19:08:49 -0300

    fix: remove unnused exports

--- app/components/index.ts
+++ app/components/index.ts
@@ -3,6 +3,4 @@ export * from './OnboardingMask/OnboardingMask';
 export * from './OnboardingPagination/OnboardingPagination';
 export * from './OnboardingButton/OnboardingButton';
 export * from './OnboardingImages/OnboardingImages';
-export * from './BottomTab/BottomTab';
-export * from './BottomTabButton/BottomTabButton';
 export * from './QuoteShareImage/QuoteShareImage';
