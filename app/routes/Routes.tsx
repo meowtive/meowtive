5f301bbb586afd2c044699b78f926a74c91efa77
filipirafael.123@gmail.com <filipirafael.123@gmail.com>
2024-12-30 18:53:45 -0300

    feat: use react-native-motion-tabs instead of local bottom-tabs

--- app/routes/Routes.tsx
+++ app/routes/Routes.tsx
@@ -1,23 +1,48 @@
 /* eslint-disable react/no-unstable-nested-components */
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { createMotionTabs } from 'react-native-motion-tabs';
 
 import { HomeScreen, FavoritesScreen, ProfileScreen } from '@screens';
-import { BottomTab } from '@components';
 
-const Tab = createBottomTabNavigator();
+const Tabs = createMotionTabs({
+  tabs: [
+    {
+      name: 'Home',
+      component: HomeScreen,
+      icon: 'quote',
+      iconType: 'Entypo',
+    },
+    {
+      name: 'Theme',
+      component: FavoritesScreen,
+      icon: 'color-palette-outline',
+      iconType: 'Ionicons',
+    },
+    {
+      name: 'Favorite',
+      component: FavoritesScreen,
+      icon: 'heart-outline',
+      iconType: 'Ionicons',
+    },
+    {
+      name: 'Profile',
+      component: ProfileScreen,
+      icon: 'paw',
+      iconType: 'Ionicons',
+    },
+  ],
+  style: {
+    activeButton: '#EF7E06',
+    activeText: '#FFFFFF',
+    inactiveText: '#000000',
+    backgroundColor: '#FFFFFF',
+  },
+});
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{ headerShown: false }}
-        tabBar={props => <BottomTab {...props} />}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Theme" component={FavoritesScreen} />
-        <Tab.Screen name="Favorite" component={FavoritesScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+      <Tabs />
     </NavigationContainer>
   );
 }
