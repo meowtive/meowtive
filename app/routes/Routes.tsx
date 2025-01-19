f1a011ee9385bf7e933d2130b28915a0723115f7
filipirafael.123@gmail.com <filipirafael.123@gmail.com>
2025-01-19 14:04:43 -0300

    fix: move translation hook from out to inside component

--- app/routes/Routes.tsx
+++ app/routes/Routes.tsx
@@ -10,44 +10,44 @@ import {
   ThemeScreen,
 } from '@screens';
 
-const { t } = useTranslation();
-
-const Tabs = createMotionTabs({
-  tabs: [
-    {
-      name: t('homeBottomTab'),
-      component: HomeScreen,
-      icon: 'quote',
-      iconType: 'Entypo',
-    },
-    {
-      name: t('favoritesBottomTab'),
-      component: FavoritesScreen,
-      icon: 'heart-outline',
-      iconType: 'Ionicons',
-    },
-    {
-      name: t('themeBottomTab'),
-      component: ThemeScreen,
-      icon: 'color-palette-outline',
-      iconType: 'Ionicons',
-    },
-    {
-      name: t('profileBottomTab'),
-      component: ProfileScreen,
-      icon: 'paw',
-      iconType: 'Ionicons',
-    },
-  ],
-  style: {
-    activeButton: '#EF7E06',
-    activeText: '#FFFFFF',
-    inactiveText: '#000000',
-    backgroundColor: '#FFFFFF',
-  },
-});
-
 export default function Routes() {
+  const { t } = useTranslation();
+
+  const Tabs = createMotionTabs({
+    tabs: [
+      {
+        name: t('homeBottomTab'),
+        component: HomeScreen,
+        icon: 'quote',
+        iconType: 'Entypo',
+      },
+      {
+        name: t('favoritesBottomTab'),
+        component: FavoritesScreen,
+        icon: 'heart-outline',
+        iconType: 'Ionicons',
+      },
+      {
+        name: t('themeBottomTab'),
+        component: ThemeScreen,
+        icon: 'color-palette-outline',
+        iconType: 'Ionicons',
+      },
+      {
+        name: t('profileBottomTab'),
+        component: ProfileScreen,
+        icon: 'paw',
+        iconType: 'Ionicons',
+      },
+    ],
+    style: {
+      activeButton: '#EF7E06',
+      activeText: '#FFFFFF',
+      inactiveText: '#000000',
+      backgroundColor: '#FFFFFF',
+    },
+  });
+
   return (
     <NavigationContainer>
       <Tabs />
