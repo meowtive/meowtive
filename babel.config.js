cb2dc91f8d626aa87e1976e7b87b72cb31b4b252
gustavoabel <gustavo.abel@totvs.com.br>
2025-02-05 00:53:19 -0300

    feat: setup react-native-paper and add provider

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
+    ['react-native-paper/babel'],
     [
       'module-resolver',
       {
